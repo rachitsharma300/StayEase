994ba05d25a13a981c476639fe830b3d0dff830b
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-12 00:11:17 +0530

    init: Backend stayease

---
+++ stayease-backend/src/test/java/com/stayease/backend/StayEaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stayease.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StayEaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
