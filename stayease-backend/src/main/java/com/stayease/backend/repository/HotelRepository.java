f7d7ffb0766577d16568797f8361868bae9e4fa7
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-13 23:18:16 +0530

    HotelRepository.java - repository for hotels

--- stayease-backend/src/main/java/com/stayease/backend/repository/HotelRepository.java
+++ stayease-backend/src/main/java/com/stayease/backend/repository/HotelRepository.java
@@ -2,9 +2,13 @@ package com.stayease.backend.repository;
 
 import com.stayease.backend.model.Hotel;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface HotelRepository extends JpaRepository<Hotel, Long> {
-    List<Hotel> findByAddressContainingIgnoreCase(String location);
+    List<Hotel> findByAddressContainingIgnoreCaseOrCityContainingIgnoreCase(String address, String city);
+
+    @Query("SELECT h FROM Hotel h WHERE LOWER(h.address) LIKE LOWER(CONCAT('%', :location, '%')) OR LOWER(h.city) LIKE LOWER(CONCAT('%', :location, '%'))")
+    List<Hotel> searchByLocation(@Param("location") String location);
 }
