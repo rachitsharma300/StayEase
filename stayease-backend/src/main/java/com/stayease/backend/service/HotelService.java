70e5c5a19d91340c39ba8643456a59b77366ef70
rachitsharma300 <rachitsharma300@gmail.com>
2025-11-13 23:18:17 +0530

    HotelService.java - hotel service interface

--- stayease-backend/src/main/java/com/stayease/backend/service/HotelService.java
+++ stayease-backend/src/main/java/com/stayease/backend/service/HotelService.java
@@ -1,7 +1,6 @@
 package com.stayease.backend.service;
 
 import com.stayease.backend.model.Hotel;
-
 import java.util.List;
 
 public interface HotelService {
@@ -9,5 +8,7 @@ public interface HotelService {
     Hotel update(Long id, Hotel hotel);
     void delete(Long id);
     Hotel getById(Long id);
+    List<Hotel> getAll();
     List<Hotel> searchByLocation(String location);
+    List<Hotel> searchHotels(String location, Double minPrice, Double maxPrice, Double minRating);
 }
